3d66be068a04eb64e706abdc836bf03fe229b8cf
Igor Mandello <igormandello@gmail.com>
2021-10-29 19:15:51 -0300

    grh

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,25 @@
+use std::iter::Cycle;
+use crate::twitter::{Tweet, TwitterClient};
+
 pub struct App {
     pub running: bool,
-    pub current_tweet: Option<String>,
+    pub tweets: Vec<Tweet>,
+    pub current_tweet: usize,
 }
 
 impl App {
     pub fn new() -> App {
         App {
             running: true,
-            current_tweet: Option::None,
+            tweets: TwitterClient::fetch_timeline(),
+            current_tweet: 0,
         }
     }
 
     pub fn on_key(&mut self, c: char) {
         match c {
             'q' => self.running = false,
-            'p' => self.current_tweet = Option::Some(String::from("The library is based on the principle of immediate rendering with intermediate buffers. This means that at each new frame you should build all widgets that are supposed to be part of the UI. While providing a great flexibility for rich and interactive UI, this may introduce over.")),
+            'p' => self.current_tweet += 1,
             _ => {}
         }
     }
